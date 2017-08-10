980b22ee8d5cb94089109525528861177a2602b1
surajcse116 <suraj.shaw@esolzmail.com>
2017-08-10 17:07:53 +0530

    10 aug

---
+++ app/src/main/java/com/android/llc/proringer/pro/proringerpro/fragmnets/bottomNav/MyProjectsFragment.java
@@ -0,0 +1,63 @@
+package com.android.llc.proringer.pro.proringerpro.fragmnets.bottomNav;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.android.llc.proringer.pro.proringerpro.R;
+import com.android.llc.proringer.pro.proringerpro.activities.LandScreenActivity;
+
+import java.util.List;
+
+/**
+ * Created by bodhidipta on 12/06/17.
+ * <!-- * Copyright (c) 2017, Proringer-->
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -->
+ */
+
+public class MyProjectsFragment extends Fragment {
+
+    RecyclerView project_list;
+    LinearLayout no_project_available, LLNetworkDisconnection;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.my_projects, container, false);
+    }
+
+    @Override
+    public void onViewCreated(final View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        no_project_available = (LinearLayout) view.findViewById(R.id.no_project_available);
+        LLNetworkDisconnection = (LinearLayout) view.findViewById(R.id.LLNetworkDisconnection);
+        project_list = (RecyclerView) view.findViewById(R.id.project_list);
+        project_list.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    public interface onOptionSelected {
+        void onItemPassed(int position, String value);
+    }
+}
