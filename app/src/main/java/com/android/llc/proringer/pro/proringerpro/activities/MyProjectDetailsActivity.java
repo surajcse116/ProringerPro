176ed37a142d0a4a4610545dae382df0736d5d7c
surajcse116 <suraj.shaw@esolzmail.com>
2017-12-28 17:14:24 +0530

    suraj 28 dec

--- app/src/main/java/com/android/llc/proringer/pro/proringerpro/activities/MyProjectDetailsActivity.java
+++ app/src/main/java/com/android/llc/proringer/pro/proringerpro/activities/MyProjectDetailsActivity.java
@@ -1,24 +1,41 @@
 package com.android.llc.proringer.pro.proringerpro.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 import com.android.llc.proringer.pro.proringerpro.R;
 import com.android.llc.proringer.pro.proringerpro.appconstant.ProConstant;
 import com.android.llc.proringer.pro.proringerpro.helper.CustomJSONParser;
+import com.android.llc.proringer.pro.proringerpro.helper.CustomMapView;
+import com.android.llc.proringer.pro.proringerpro.helper.HelperClass;
 import com.android.llc.proringer.pro.proringerpro.helper.Logger;
 import com.android.llc.proringer.pro.proringerpro.helper.MyLoader;
 import com.android.llc.proringer.pro.proringerpro.helper.ProApplication;
 import com.android.llc.proringer.pro.proringerpro.pojo.SetGetAPI;
 import com.android.llc.proringer.pro.proringerpro.viewsmod.textview.ProRegularTextView;
 import com.bumptech.glide.Glide;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolygonOptions;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,15 +47,19 @@ import java.util.ArrayList;
  * Created by su on 8/18/17.
  */
 
-public class MyProjectDetailsActivity extends AppCompatActivity {
+public class MyProjectDetailsActivity extends AppCompatActivity implements OnMapReadyCallback {
     int screenHeight;
     int screenWidth;
-    String project_id="";
-    ImageView img_project, img_map,img_likes;
+    String project_id = "", zip = "";
+    ImageView img_project, img_likes;
+    CustomMapView mapview;
     RelativeLayout RLImage;
-
+    GoogleMap mMap;
     ArrayList<SetGetAPI> arrayList = null;
     MyLoader myload;
+    double slat,slong,northlat,northlong;
+    int REQ_PERMISSION=1000;
+    ScrollView scrollView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -52,27 +73,38 @@ public class MyProjectDetailsActivity extends AppCompatActivity {
 
         myload = new MyLoader(MyProjectDetailsActivity.this);
 
-        project_id=getIntent().getStringExtra("project_id");
+        project_id = getIntent().getStringExtra("project_id");
 
         DisplayMetrics displaymetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
 
-        screenHeight =displaymetrics.heightPixels;
-        screenWidth =displaymetrics.widthPixels;
+        screenHeight = displaymetrics.heightPixels;
+        screenWidth = displaymetrics.widthPixels;
 
+        scrollView = (ScrollView) findViewById(R.id.scrollView);
         RLImage = (RelativeLayout) findViewById(R.id.RLImage);
         img_project = (ImageView) findViewById(R.id.img_project);
-        img_map = (ImageView) findViewById(R.id.img_map);
         img_likes = (ImageView) findViewById(R.id.img_likes);
+        mapview = (CustomMapView) findViewById(R.id.mapview);
+
+        mapview.onCreate(Bundle.EMPTY);
+        mapview.getMapAsync(this);
+        MapsInitializer.initialize(this);
 
 
+        RLImage.getLayoutParams().height = (int) 3 * (screenWidth - 20) / 4;
+        RLImage.getLayoutParams().width = screenWidth - 20;
 
-        RLImage.getLayoutParams().height = (int) 3*(screenWidth - 20)/4;
-        RLImage.getLayoutParams().width = screenWidth-20;
-
-        img_map.getLayoutParams().height = (int) 2*(screenWidth - 10) / 5;
+        mapview.getLayoutParams().height = (int) 2 * (screenWidth - 10) / 5;
 
         showData();
+
+        mapview.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
 
     }
 
@@ -104,29 +136,32 @@ public class MyProjectDetailsActivity extends AppCompatActivity {
                 Logger.printMessage("Result", result);
 
                 try {
-                    JSONArray info_array=new JSONObject(result).getJSONArray("info_array");
-                    JSONObject jsonObject=info_array.getJSONObject(0);
+                    JSONArray info_array = new JSONObject(result).getJSONArray("info_array");
+                    JSONObject jsonObject = info_array.getJSONObject(0);
 
-                    ((ProRegularTextView)findViewById(R.id.tv_posted_by_value)).setText(jsonObject.getString("posted_by"));
-                    ((ProRegularTextView)findViewById(R.id.tv_posted_date)).setText(jsonObject.getString("post_date"));
-                    ((ProRegularTextView)findViewById(R.id.tv_address)).setText(jsonObject.getString("address"));
+                    ((ProRegularTextView) findViewById(R.id.tv_posted_by_value)).setText(jsonObject.getString("posted_by"));
+                    ((ProRegularTextView) findViewById(R.id.tv_posted_date)).setText(jsonObject.getString("post_date"));
+                    ((ProRegularTextView) findViewById(R.id.tv_address)).setText(jsonObject.getString("address"));
 
                     Glide.with(MyProjectDetailsActivity.this).load(jsonObject.getString("project_image")).into(img_project);
-                    ((ProRegularTextView)findViewById(R.id.tv_project_title)).setText(jsonObject.getString("project_name"));
-                    ((ProRegularTextView)findViewById(R.id.tv_project_title)).setText(jsonObject.getString("project_name"));
-                    ((ProRegularTextView)findViewById(R.id.tv_type_of_work)).setText(jsonObject.getString("type_of_work"));
-                    ((ProRegularTextView)findViewById(R.id.tv_service)).setText(jsonObject.getString("service"));
-                    ((ProRegularTextView)findViewById(R.id.tv_property)).setText(jsonObject.getString("property"));
-                    ((ProRegularTextView)findViewById(R.id.tv_phone)).setText(jsonObject.getString("phone"));
-                    ((ProRegularTextView)findViewById(R.id.tv_describetion)).setText(jsonObject.getString("job_details"));
+                    ((ProRegularTextView) findViewById(R.id.tv_project_title)).setText(jsonObject.getString("project_name"));
+                    ((ProRegularTextView) findViewById(R.id.tv_project_title)).setText(jsonObject.getString("project_name"));
+                    ((ProRegularTextView) findViewById(R.id.tv_type_of_work)).setText(jsonObject.getString("type_of_work"));
+                    ((ProRegularTextView) findViewById(R.id.tv_service)).setText(jsonObject.getString("service"));
+                    ((ProRegularTextView) findViewById(R.id.tv_property)).setText(jsonObject.getString("property"));
+                    ((ProRegularTextView) findViewById(R.id.tv_phone)).setText(jsonObject.getString("phone"));
+                    ((ProRegularTextView) findViewById(R.id.tv_describetion)).setText(jsonObject.getString("job_details"));
 
-                    if (jsonObject.getString("add_favouite").equalsIgnoreCase("0")){
+                    zip = jsonObject.getString("zip");
+
+                    if (jsonObject.getString("add_favouite").equalsIgnoreCase("0")) {
 //                        img_likes.setImageResource(R.drawable.ic_unfavorite);
                         Glide.with(MyProjectDetailsActivity.this).load("").placeholder(R.drawable.ic_unfavorite).into(img_likes);
-                    }else {
+                    } else {
 //                        img_likes.setImageResource(R.drawable.ic_favorite);
                         Glide.with(MyProjectDetailsActivity.this).load("").placeholder(R.drawable.ic_favorite).into(img_likes);
                     }
+                    showlatlong();
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -168,4 +203,159 @@ public class MyProjectDetailsActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        setUpMap();
+    }
+
+
+    @Override
+    protected void onResume() {
+        mapview.onResume();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        mapview.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mapview.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        mapview.onLowMemory();
+        super.onLowMemory();
+    }
+
+    public void showlatlong() {
+        HelperClass.getInstance(MyProjectDetailsActivity.this).getlatlongAPI(new HelperClass.getApiProcessCallback() {
+            @Override
+            public void onStart() {
+
+            }
+
+            @Override
+            public void onComplete(String message) {
+                Logger.printMessage("valu", message);
+                try {
+                    JSONObject jo = new JSONObject(message);
+                    Logger.printMessage("infoarry", String.valueOf(jo));
+                    if (jo.getString("status").equalsIgnoreCase("OK") && jo.has("results") &&
+                            jo.getJSONArray("results").length() > 0) {
+                        JSONArray results = jo.getJSONArray("results");
+
+                        for (int i = 0; i < results.length(); i++) {
+
+                            Logger.printMessage("totalvalue", String.valueOf(results.getJSONObject(i)));
+
+                            Logger.printMessage("totalvalu", String.valueOf(results.getJSONObject(i).getJSONObject("geometry")));
+                            JSONObject location = results.getJSONObject(i).getJSONObject("geometry").getJSONObject("location");
+                            Logger.printMessage("loc", String.valueOf(location));
+//                            locationlat = Double.parseDouble(location.getString("lat"));
+//                            locationlong = Double.parseDouble(location.getString("lng"));
+//                            Logger.printMessage("locationlat", String.valueOf(locationlat));
+//                            Logger.printMessage("locationlong", String.valueOf(locationlong));
+                            JSONObject view = results.getJSONObject(i).getJSONObject("geometry").getJSONObject("viewport");
+                            Logger.printMessage("view", String.valueOf(view));
+                            JSONObject north = view.getJSONObject("northeast");
+                            northlat = Double.parseDouble(north.getString("lat"));
+                            northlong = Double.parseDouble(north.getString("lng"));
+
+                            // mMap.addMarker(new MarkerOptions().position(new LatLng(locationlat,locationlong)).title("service area"));
+                            Logger.printMessage("northlat", String.valueOf(northlat));
+                            Logger.printMessage("northlong", String.valueOf(northlong));
+                            JSONObject sw = view.getJSONObject("southwest");
+                            slat = Double.parseDouble(sw.getString("lat"));
+                            slong = Double.parseDouble(sw.getString("lng"));
+
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(northlat, northlong), 9));
+
+                            Logger.printMessage("slat", String.valueOf(slat));
+                            Logger.printMessage("slong", String.valueOf(slong));
+
+
+                            PolygonOptions rectOptions = new PolygonOptions()
+                                    .add(new LatLng(northlat, northlong))
+                                    .add(new LatLng(slat, northlong))  // North of the previous point, but at the same longitude
+                                    .add(new LatLng(slat, slong))  // Same latitude, and 30km to the west
+                                    .add(new LatLng(northlat, slong))  // Same longitude, and 16km to the south
+                                    .add(new LatLng(northlat, northlong)).strokeColor(Color.RED).fillColor(Color.rgb(226, 173, 173)).strokeWidth(5);
+                            // Closes the polyline.
+
+                            mMap.addPolygon(rectOptions);
+                        }
+
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onError(String error) {
+
+            }
+        }, zip);
+    }
+
+    public void setUpMap() {
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if ((ContextCompat.checkSelfPermission(MyProjectDetailsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)) {
+                mMap.setMyLocationEnabled(true);
+            } else {
+                ActivityCompat.requestPermissions(MyProjectDetailsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQ_PERMISSION);
+            }
+        }else {
+            mMap.setMyLocationEnabled(true);
+        }
+
+        // mMap.setMyLocationEnabled(true);
+        mMap.setTrafficEnabled(true);
+        mMap.setIndoorEnabled(true);
+        mMap.setBuildingsEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        Log.d("Permission-->", "onRequestPermissionsResult()");
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1000: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted
+                    if (checkPermission())
+                        mMap.setMyLocationEnabled(true);
+
+                } else {
+                    // Permission denied
+
+                }
+                break;
+            }
+        }
+    }
+
+    // Check for permission to access Location
+    private boolean checkPermission() {
+        Log.d("checkPermission-->", "checkPermission()");
+        // Ask for permission if it wasn't granted yet
+        return (ContextCompat.checkSelfPermission(MyProjectDetailsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED );
+    }
 }
---
+++ app/src/main/java/com/android/llc/proringer/pro/proringerpro/helper/CustomMapView.java
@@ -0,0 +1,34 @@
+package com.android.llc.proringer.pro.proringerpro.helper;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+/**
+ * Created by su on 12/28/17.
+ */
+
+public class CustomMapView extends MapView {
+    public CustomMapView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_UP:
+                System.out.println("unlocked");
+                this.getParent().requestDisallowInterceptTouchEvent(false);
+                break;
+            case MotionEvent.ACTION_DOWN:
+                System.out.println("locked");
+                this.getParent().requestDisallowInterceptTouchEvent(true);
+                break;
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+}
