acbdf77174515897a7112ee05217f980c77c5814
surajcse116 <suraj.shaw@esolzmail.com>
2018-01-09 17:07:27 +0530

    suraj 9 jan

--- app/src/main/java/com/android/llc/proringer/pro/proringerpro/activities/SignUpActivity.java
+++ app/src/main/java/com/android/llc/proringer/pro/proringerpro/activities/SignUpActivity.java
@@ -1,6 +1,5 @@
 package com.android.llc.proringer.pro.proringerpro.activities;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,7 +8,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.inputmethod.InputMethodManager;
 
 import com.android.llc.proringer.pro.proringerpro.R;
 import com.android.llc.proringer.pro.proringerpro.fragmnets.registrationfragment.RegistrationOne;
