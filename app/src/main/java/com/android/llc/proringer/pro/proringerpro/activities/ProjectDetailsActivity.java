773a1296adfcc88e4125a0483054b2d1b986454f
surajcse116 <suraj.shaw@esolzmail.com>
2018-01-11 14:59:59 +0530

    suraj 11 jan

--- app/src/main/java/com/android/llc/proringer/pro/proringerpro/activities/ProjectDetailsActivity.java
+++ app/src/main/java/com/android/llc/proringer/pro/proringerpro/activities/ProjectDetailsActivity.java
@@ -232,7 +232,11 @@ public class ProjectDetailsActivity extends AppCompatActivity implements OnMapRe
                             Logger.printMessage("SpanHello", "click");
 
                             try {
-                                new ShowMyDialog(ProjectDetailsActivity.this).showDescribetionDialog("Phone Number", jsonObject.getString("phone").trim());
+                                if (true) {
+                                    new ShowMyDialog(ProjectDetailsActivity.this).showDescribetionDialog("Phone Number", jsonObject.getString("phone").trim());
+                                }else {
+                                    Toast.makeText(ProjectDetailsActivity.this,"You have not a Premium User",Toast.LENGTH_SHORT).show();
+                                }
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
