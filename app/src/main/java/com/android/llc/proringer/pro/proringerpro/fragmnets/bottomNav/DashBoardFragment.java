b049537a8e6c2104728149f200c5acb2c3c36c75
surajcse116 <suraj.shaw@esolzmail.com>
2018-01-13 17:51:43 +0530

    suraj 13 jan

--- app/src/main/java/com/android/llc/proringer/pro/proringerpro/fragmnets/bottomNav/DashBoardFragment.java
+++ app/src/main/java/com/android/llc/proringer/pro/proringerpro/fragmnets/bottomNav/DashBoardFragment.java
@@ -14,7 +14,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.RelativeLayout;
