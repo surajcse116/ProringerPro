bde886efdb993d11765fa5e8e31c3df4e746702b
surajcse116 <suraj.shaw@esolzmail.com>
2018-01-16 12:31:11 +0530

    suraj 15 jan

---
+++ app/src/main/java/com/android/llc/proringer/pro/proringerpro/pushNotification/MyFirebaseInstanceIDService.java
@@ -0,0 +1,68 @@
+package com.android.llc.proringer.pro.proringerpro.pushNotification;
+
+import android.util.Log;
+
+import com.android.llc.proringer.pro.proringerpro.appconstant.ProConstant;
+import com.android.llc.proringer.pro.proringerpro.helper.CustomJSONParser;
+import com.android.llc.proringer.pro.proringerpro.helper.ProApplication;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import java.util.HashMap;
+
+/**
+ * Created by apple on 23/05/17.
+ */
+
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("@@ ", "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+    // [END refresh_token]
+
+    private void sendRegistrationToServer(String token) {
+        HashMap<String, String> Params = new HashMap<>();
+        Params.put("user_id",  ProApplication.getInstance().getUserId());
+        Params.put("anorid_device_id", token);
+
+        new CustomJSONParser().fireAPIForPostMethod(getApplication(), ProConstant.BASEURL + "users_device_update", Params, null, new CustomJSONParser.CustomJSONResponse() {
+            @Override
+            public void onSuccess(String result) {
+
+            }
+
+            @Override
+            public void onError(String error, String response) {
+
+            }
+
+            @Override
+            public void onError(String error) {
+
+            }
+
+            @Override
+            public void onStart() {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/android/llc/proringer/pro/proringerpro/pushNotification/MyFirebaseMessagingService.java
@@ -0,0 +1,183 @@
+package com.android.llc.proringer.pro.proringerpro.pushNotification;
+
+import android.app.ActivityManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.android.llc.proringer.pro.proringerpro.R;
+import com.android.llc.proringer.pro.proringerpro.helper.Logger;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+//import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+//import com.firebase.jobdispatcher.GooglePlayDriver;
+//import com.firebase.jobdispatcher.Job;
+
+/**
+ * Created by apple on 23/05/17.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "MyFirebaseMsgService";
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    // [START receive_message]
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+//        // [START_EXCLUDE]
+//        // There are two types of messages data messages and notification messages. Data messages are handled
+//        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
+//        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
+//        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
+//        // When the user taps on the notification they are returned to the app. Messages containing both notification
+//        // and data payloads are treated as notification messages. The Firebase console always sends notification
+//        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
+//        // [END_EXCLUDE]
+//
+//        // TODO(developer): Handle FCM messages here.
+//        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+//        Log.d(TAG, "From: " + remoteMessage.getFrom());
+//
+//        // Check if message contains a data payload.
+//        if (remoteMessage.getData().size() > 0) {
+//            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+//
+//            if (/* Check if data needs to be processed by long running job */ true) {
+//                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+//                scheduleJob();
+//            } else {
+//                // Handle message within 10 seconds
+//                handleNow();
+//            }
+//
+//        }
+//
+//        // Check if message contains a notification payload.
+//        if (remoteMessage.getNotification() != null) {
+//            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+//        }
+//
+//        // Also if you intend on generating your own notifications as a result of a received FCM
+//        // message, here is where that should be initiated. See sendNotification method below.
+
+        Logger.printMessage("@@@ PUSH", remoteMessage.getData().toString());
+        try {
+            JSONObject Object = new JSONObject(remoteMessage.getData().get("body"));
+
+            Log.d("PushResponse", "==" + Object.toString());
+            sendNotification(Object.toString());
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+    // [END receive_message]
+
+    /**
+     * Schedule a job using FirebaseJobDispatcher.
+     */
+    private void scheduleJob() {
+        // [START dispatch_job]
+//        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(this));
+//        Job myJob = dispatcher.newJobBuilder()
+//                .setService(MyJobService.class)
+//                .setTag("my-job-tag")
+//                .build();
+//        dispatcher.schedule(myJob);
+        // [END dispatch_job]
+    }
+
+    /**
+     * Handle time allotted to BroadcastReceivers.
+     */
+    private void handleNow() {
+        Log.d(TAG, "Short lived task is done.");
+    }
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param messageBody FCM message body received.
+     */
+    private void sendNotification(String messageBody) {
+
+        try {
+
+            Bitmap icon = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher_pro);
+
+            //Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+//            Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+//            if(defaultSoundUri == null){
+//                defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+//                if(defaultSoundUri == null){
+//                    defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+//                }
+//            }
+
+            NotificationCompat.BigTextStyle bigText = new NotificationCompat.BigTextStyle();
+            bigText.bigText(messageBody);
+            bigText.setBigContentTitle(messageBody);
+
+
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                    .setLargeIcon(icon)
+                    .setSmallIcon(R.mipmap.ic_launcher_pro)
+                    .setContentTitle("ProringerPro")
+                    .setContentText(messageBody)
+                    .setAutoCancel(true)
+                    .setDefaults(Notification.DEFAULT_ALL)
+
+                    .setStyle(bigText)
+
+//                     //Vibration
+//                    .setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 })
+//
+//                    //LED
+//                    .setLights(Color.GREEN, 3000, 3000)
+
+                     //Ton
+                    //.setSound(Uri.parse("uri://sadfasdfasdf.mp3"))
+                    //.setSound(defaultSoundUri)
+
+                    .setPriority(Notification.PRIORITY_HIGH);
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean applicationInForeground() {
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> services = activityManager.getRunningAppProcesses();
+        boolean isActivityFound = false;
+
+        if (services.get(0).processName.equalsIgnoreCase(getPackageName())) {
+            isActivityFound = true;
+        }
+        return isActivityFound;
+    }
+}
